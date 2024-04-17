10d6959f0ff2b90fc00a0eb28ec43f9eddbfc9ed
Jacob Bates <jhb0020@gmail.com>
2024-04-17 15:58:10 -0500

    update test data path

--- tests/test_geom.py
+++ tests/test_geom.py
@@ -4,7 +4,7 @@ import h5py
 from pyproj import CRS
 from pathlib import Path
 
-TEST_DATA = Path("./data")
+TEST_DATA = Path("./tests/data")
 
 def test_projection(tmp_path):
     wkt = 'PROJCS["Albers_Conic_Equal_Area",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Albers"],PARAMETER["false_easting",0.0],PARAMETER["false_northing",0.0],PARAMETER["central_meridian",-96.0],PARAMETER["standard_parallel_1",29.5],PARAMETER["standard_parallel_2",45.5],PARAMETER["latitude_of_origin",37.5],UNIT["Foot_US",0.3048006096012192]]'
