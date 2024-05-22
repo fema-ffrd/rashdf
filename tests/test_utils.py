274e68809e835ee4eb93e92aedb50d8de44c4007
Stevenray Janke <sjanke@dewberry.com>
2024-05-22 16:58:38 -0400

    Edit datetime parse functions to handle time of 2400

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,9 +14,16 @@ def test_convert_ras_hdf_value():
     assert utils.convert_ras_hdf_value(b"15Mar2024 16:39:01") == datetime(
         2024, 3, 15, 16, 39, 1
     )
+    assert utils.convert_ras_hdf_value(b"15Mar2024 24:00:00") == datetime(
+        2024, 3, 16, 0, 0, 0
+    )
     assert utils.convert_ras_hdf_value(b"15Mar2024 16:39:01 to 16Mar2024 16:39:01") == [
         datetime(2024, 3, 15, 16, 39, 1),
         datetime(2024, 3, 16, 16, 39, 1),
+    ]
+    assert utils.convert_ras_hdf_value(b"18Mar2024 24:00:00 to 19Mar2024 24:00:00") == [
+        datetime(2024, 3, 19, 0, 0, 0),
+        datetime(2024, 3, 20, 0, 0, 0),
     ]
     assert utils.convert_ras_hdf_value(b"01:23:45") == timedelta(
         hours=1, minutes=23, seconds=45
